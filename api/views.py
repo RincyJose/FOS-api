8ad42a9be3b29f7ce77d526a3f974cf05bc817d3
Rincy Jose <rincyjosec388@gmail.com>
2022-06-23 15:11:08 +0530

    Ordering api

--- api/views.py
+++ api/views.py
@@ -6,15 +6,23 @@ from rest_framework.views import APIView
 from .models import Question
 from .serializers import QuestionSerializer
 from rest_framework import filters
-from django_filters import rest_framework as filters
-
+from rest_framework.filters import OrderingFilter
+# from django_filters import rest_framework as filters
 
 
 class QuestionsAPIView(generics.ListCreateAPIView):
     queryset = Question.objects.all()
-    filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ('question_text','author')
     serializer_class = QuestionSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
+    
+
+# filtering
+# class QuestionsAPIView(generics.ListCreateAPIView):
+#     queryset = Question.objects.all()
+#     filter_backends = (filters.DjangoFilterBackend,)
+#     filterset_fields = ('question_text','author')
+#     serializer_class = QuestionSerializer
     
 # Search filter    
 # class QuestionsAPIView(generics.ListCreateAPIView):
